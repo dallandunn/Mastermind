21da22bd8381fb54873ed20b9dd0fc2ec903a231
Dallan Dunn <dallanpdunn@gmail.com>
2022-03-27 11:04:48 -0400

    Add computer guessing

--- mastermind.rb
+++ mastermind.rb
@@ -8,11 +8,37 @@ class Computer
         @colors = possible_colors
     end
 
-    def code_maker
+    def make_code
         @secret_code = []
         for i in (0...4) do
             @secret_code.push(@colors.sample)
         end
+    end
+
+    def break_code(matches=[])
+        if matches == []
+            make_code
+        else
+            prev_code = @secret_code.map(&:clone)
+            part_matches = []
+            matches.each_with_index do |match, index|
+                if match == "M"
+                    @secret_code[index] = prev_code[index]
+                elsif match == "P"
+                    part_matches.push(prev_code[index])
+                else
+                    @secret_code[index] = @colors.sample
+                end
+            end
+
+            matches.each_with_index do |match, index|
+                if match == "P"
+                    @secret_code[index] = part_matches.sample
+                end
+            end
+        end
+        puts "The computer guesses:"
+        puts "#{@secret_code}"
     end
 end
 
@@ -23,6 +49,15 @@ class Game
         @cpu = computer
         @num_turns = num_turns
         @colors = possible_colors
+        @results = []
+        @player_role = 0
+        puts "Welcome to Mastermind!"
+        puts "In this game you can either make a secret code for the computer to guess or you can guess the computer's secret code."
+        while @player_role != 1 && @player_role != 2 do
+            puts "Press 1 to be the Code Maker"
+            puts "Press 2 to be the Code Breaker"
+            @player_role = gets.chomp.to_i
+        end
     end
 
     private
@@ -32,7 +67,7 @@ class Game
         puts "Enter a code of 4 colors (press enter after each color):"
         for i in (0...4) do
             while not @colors.include?(@user_code[i]) do
-                @user_code[i] = gets.chomp
+                @user_code[i] = gets.chomp.downcase
                 if not @colors.include?(@user_code[i])
                     print "Not a color in the list. Try again: "
                 end
@@ -42,30 +77,52 @@ class Game
         puts "#{@user_code}"
     end
 
-    def compare_guess(secret_code)
-        results = []
+    def compare_guess(secret_code, guess_code)
+        secret = secret_code.map(&:clone)
+        @results = ["?", "?", "?", "?"]
         exact_matches = 0
         partial_matches = 0
         for i in (0...4) do
-            if @user_code[i] == secret_code[i]
-                results.push("M")
+            if guess_code[i] == secret_code[i]
+                secret[i] = "M"
+                @results[i] = ("M")
                 exact_matches += 1
-            elsif secret_code.include?(@user_code[i])
-                results.push("P")
-                partial_matches += 1
-            else
-                results.push("X")
             end
         end
-        puts "#{results}"
-        puts "Exact Matches: #{exact_matches}"
-        puts "Partial Matches: #{partial_matches}"
+
+        for i in (0...4) do
+            if secret.include?(guess_code[i]) && secret[i] != "M"
+                @results[i] = ("P")
+                partial_matches += 1
+            end
+        end
+        puts "#{@results}"
+        puts "Exact Matches (M): #{exact_matches}"
+        puts "Partial Matches (P): #{partial_matches}"
     end
 
-    public
+    def code_maker
+        puts "Create a code using 4 of the following colors."
+        puts "#{@colors}"
+        get_user_code
+        while @num_turns > 0
+            puts "The computer has #{@num_turns} guesses remaining"
+            @cpu.break_code(@results)
+            compare_guess(@user_code, @cpu.secret_code)
+            if @user_code == @cpu.secret_code
+                puts "Game 0ver! The computer guessed the code!"
+                break
+            end
+            
+            print "press [enter] to see the computer's next guess."
+            gets.chomp
+            puts "-------------------------"
+            @num_turns -= 1
+        end
+    end
 
-    def play
-        @cpu.code_maker
+    def code_breaker
+        @cpu.make_code
         puts "Guess the computer's code from these possible colors:"
         puts "#{@colors}"
 
@@ -73,13 +130,24 @@ class Game
             puts "You have #{@num_turns} guesses remaining."
             results = Array.new(4)
             get_user_code
-            compare_guess(@cpu.secret_code)
+            compare_guess(@cpu.secret_code, @user_code)
             if @user_code == @cpu.secret_code
+                puts "You guessed the code!"
                 break
             end
 
             puts "-------------------------"
             @num_turns -=1
+       end
+    end
+
+    public
+
+    def play
+       if @player_role == 1
+            code_maker
+       else
+            code_breaker
        end
     end
 end
